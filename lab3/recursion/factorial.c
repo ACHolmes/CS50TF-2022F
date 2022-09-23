04a1b16f8bb5f19726011d64bc0c52987e181969
Andrew Holmes <acholmes001@gmail.com>
2022-09-22 23:19:51 -0400

    Recursion exercises added

---
+++ lab3/recursion/factorial.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int factorial(int n);
+int factorial_iter(int n);
+
+int main(void)
+{
+    printf("Recursive method, 5! = %i\n", factorial(5));
+    printf("Iterative method, 5! = %i\n", factorial_iter(5));
+}
+
+int factorial(int n)
+{
+    // Implement a recursive function that returns n!
+    return 1;
+}
+
+int factorial_iter(int n)
+{
+    int result = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
---
+++ lab3/recursion/fib.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int fib_n(int n);
+
+int main(void)
+{
+    printf("The 5th Fibonacci number is: ");
+    printf("%i\n", fib_n(5));
+}
+
+int fib_n(int n)
+{
+    // Your code here
+    // Write a function that returns the nth Fibonacci number
+    // Desired functionality: 
+    // fib(0): prints 1
+    // fib(1) prints 1
+    // fib(2) prints 2 etc
+    return 1;
+}
